361a492785519e6dd9645ddc3b1f7a0e480f01b0
Blick <45530302+Blick7@users.noreply.github.com>
2022-05-21 15:57:04 +0300

    feat: complete task 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+const dir = path.join(__dirname, '/text.txt');
+const readableStream = fs.createReadStream(dir);
+
+let data = '';
+readableStream.on('data', (chunk) => (data += chunk));
+readableStream.on('end', () => console.log(data));
